4494c5c30c02d89496963d5988dfccc51aace014
Yash Thakar <90375840+yashyaks@users.noreply.github.com>
2023-09-16 03:35:15 +0530

    commit5

--- frontend/src/components/CustomWebcam.jsx
+++ frontend/src/components/CustomWebcam.jsx
@@ -11,19 +11,53 @@ export default function CustomWebcam() {
     height: 720,
   };
 
-  const captureScreenshot = () => {
-    const screenshot = webRef.current.getScreenshot();
-    // Do something with the screenshot, e.g., save it or display it.
-    console.log('Captured screenshot:', screenshot);
-  };
+  const video = document.getElementById('webcam');
+  const liveView = document.getElementById('liveView');
+  const demosSection = document.getElementById('demos');
+  const enableWebcamButton = document.getElementById('webcamButton');
+  // Check if webcam access is supported.
+  function getUserMediaSupported() {
+    return !!(navigator.mediaDevices &&
+      navigator.mediaDevices.getUserMedia);
+  }
 
-  useEffect(() => {
-    // Set up a timer to capture a screenshot every 500ms
-    const screenshotInterval = setInterval(captureScreenshot, 500);
+  // If webcam supported, add event listener to button for when user
+  // wants to activate it to call enableCam function which we will 
+  // define in the next step.
+  if (getUserMediaSupported()) {
+    enableWebcamButton.addEventListener('click', enableCam);
+  } else {
+    console.warn('getUserMedia() is not supported by your browser');
+  }
+  // Enable the live webcam view and start classification.
+  function enableCam(event) {
+    // Only continue if the COCO-SSD has finished loading.
+    if (!model) {
+      return;
+    }
+    
+    // Hide the button once clicked.
+    event.target.classList.add('removed');  
+    
+    // getUsermedia parameters to force video but not audio.
+    const constraints = {
+      video: true
+    };
 
-    // Clean up the timer when the component unmounts
-    return () => clearInterval(screenshotInterval);
-  }, []);
+    // Activate the webcam stream.
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+      video.srcObject = stream;
+      video.addEventListener('loadeddata', predictWebcam);
+    });
+  }
+    // Placeholder function for next step.
+  function predictWebcam() {
+  }
+
+  // Pretend model has loaded so we can try out the webcam code.
+  var model = true;
+  demosSection.classList.remove('invisible');
+
 
   return (
     <div>
